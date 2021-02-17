af2c8275534aacc592b3650c9610d24aec661146
BrienB <barrdom@hotmail.com>
2021-02-16 21:27:27 -0500

    added comments

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,9 +1,13 @@
+// Function when the document is ready (i.e. all images and elements have loaded)
 $(function() {
+    // On click event listener when the button to devour a burger is clicked
     $(".devour").on("click", function(event) {
         event.preventDefault();
 
+        // Get the id of the burger record to devour from the button's custom data-id attribute 
         var id = $(this).data("id");
         
+        // Change the devoured state of the burger to true and store in newDevouredState
         var newDevouredState = {
             devoured: true
         };
@@ -14,17 +18,20 @@ $(function() {
             data: newDevouredState
         }).then(
             function() {
-                console.log("changed devoured to true");
+                console.log("Changed the devoured state to true")
                 // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
     
+    // On submit event when the submit button of the add burger form is clicked
     $(".create-form").on("submit", function(event) {
         // preventDefault on a submit event.
         event.preventDefault();
         
+        // Store the new burger name from the text input of the add burger form 
+        // as the value for burger_name field and store as object in newBurger variable
         var newBurger = {
             burger_name: $("#burger").val().trim()
         };
@@ -35,7 +42,7 @@ $(function() {
             data: newBurger
         }).then(
             function() {
-                console.log("created new burger");
+                console.log("Created new burger");
                 // Reload the page to get the updated list
                 location.reload();
             }
